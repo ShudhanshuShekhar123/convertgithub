bded31e92cdea73bdc169df524c1edde08e27248
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-09-19 13:49:24 +0530

    add

--- src/Github.js
+++ src/Github.js
@@ -3,14 +3,23 @@ import React from 'react';
 
 
 const Github = () => {
-   
-    const handleclick =()=>{
-        window.location.href= ("https://code-converter-github-integration.onrender.com/auth")
-    }
+
+  const handleclick = () => {
+    window.location.href = ("https://code-converter-github-integration.onrender.com/auth")
+  }
 
 
   return (
-   <button onClick={handleclick}>Sign IN to Github</button>
+    <div>
+      <div style={{marginTop:"10%",marginBottom:"60px"}}>
+      <h1 style={{color:"white", marginBottom:"30px",wordSpacing:"20px", fontSize:"70px"}}>CODE CONVERTER</h1>
+      <h1  style={{color:"white", textAlign:"center"}}>Convert, Debug and Elevate Your Code!</h1>
+      </div>
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginBottom:"60px"}}>
+      <button style={{padding:"10px 45px", fontSize:"22px", fontWeight:"600", borderRadius:"10px", cursor:"pointer"}} onClick={handleclick}>Sign In to Github</button>
+      </div>
+    </div>
+
   )
 }
 
